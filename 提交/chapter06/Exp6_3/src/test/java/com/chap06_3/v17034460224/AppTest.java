cdddb3a1303d2c1be938f93ae78987505118ccf1
sky <2449068617@qq.com>
2019-11-25 09:15:17 +0800

    chapter6 source code

---
+++ 提交/chapter06/Exp6_3/src/test/java/com/chap06_3/v17034460224/AppTest.java
@@ -0,0 +1,20 @@
+package com.chap06_3.v17034460224;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
